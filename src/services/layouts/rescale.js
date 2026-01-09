66f76df89f94e0be157cb953240dc89b090b60dd
jmdowrick <146781982+jmdowrick@users.noreply.github.com>
2026-01-09 14:03:29 +1300

    Introduce generalised rescaling function

--- src/services/layouts/rescale.js
+++ src/services/layouts/rescale.js
@@ -1,11 +1,33 @@
 export async function runRescaleLayout(nodes) {
     try {
+
+        // Get range of x and y values
+        let xMin = Infinity, xMax = -Infinity
+        let yMin = Infinity, yMax = -Infinity
+
+        nodes.forEach(node => {
+            xMin = Math.min(xMin, node.position.x)
+            xMax = Math.max(xMax, node.position.x)
+            yMin = Math.min(yMin, node.position.y)
+            yMax = Math.max(yMax, node.position.y)
+        })
+
+        // How spread the nodes should be
+        const TARGET_SPACING = 700
+
+        // Scale x and y independently due to typical wide aspect ratio
+        const xRange = xMax - xMin
+        const yRange = yMax - yMin
+
+        const xScale = (TARGET_SPACING * 4) / xRange
+        const yScale = TARGET_SPACING / yRange
+        
         nodes.forEach(node => {
             node.position = {
-                x: node.position.x * 400,
-                y: node.position.y * 200
+                x: (node.position.x - xRange / 2) * xScale,
+                y: (node.position.y - yRange / 2) * yScale
             }
-        });
+        })
         return true
     } catch (err) {
         console.error('Rescale Layout Failed:', err)
